f4da95e84d009ce581a4f808a4ce195e99ed5633
SadineniAbhi <abhisadineni@gmail.com>
2024-06-15 05:45:53 +0530

    reading arguments from the config files

--- replicatingtopics/src/main.py
+++ replicatingtopics/src/main.py
@@ -1,14 +1,17 @@
 from replicator import Replicator
-
+import configparser
 from kafka import KafkaConsumer, KafkaProducer
+
+config = configparser.ConfigParser()
+config.read('config.ini')
 
 
 def main(replicator: Replicator):
-    replicator.replicate('a', 'b')
+    replicator.replicate(config['topics']['from_topic'], config['topics']['to_topic'])
 
 
 if __name__ == '__main__':
-    rep_obj = Replicator(consumer_obj=KafkaConsumer(bootstrap_servers='localhost:9092'),
-                         producer_obj=KafkaProducer(bootstrap_servers='localhost:9092'))
+    rep_obj = Replicator(consumer_obj=KafkaConsumer(bootstrap_servers=config['consumer']['bootstrap_servers']),
+                         producer_obj=KafkaProducer(bootstrap_servers=config['producer']['bootstrap_servers']))
 
     main(replicator=rep_obj)
