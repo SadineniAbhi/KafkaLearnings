af00147c8dfab594ca8c9fa05f348ee72dc1f42d
Abhi <abhisadineni@gmail.com>
2024-06-16 19:46:08 +0530

    Delete avro directory
    Deleting this because this isn't reviewed yet

--- avro/src/config.py
+++
@@ -1,8 +0,0 @@
-from configparser import ConfigParser
-
-
-# reads the config from the config.ini
-def get_config(file: str) -> ConfigParser:
-    config = ConfigParser()
-    config.read(file)
-    return config
--- avro/src/read_schema.py
+++
@@ -1,4 +0,0 @@
-def get_schema(file: str) -> str:
-    with open(file) as f:
-        schema = f.read()
-    return schema
